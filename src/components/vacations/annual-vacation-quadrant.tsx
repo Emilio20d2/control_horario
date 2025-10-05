333127a4f460d747e768716c0f6ad18dc4c40b44
Emilio Gómez Pardo <emilio20d2@gmail.com>
2025-10-05 21:32:25 +0000

    Try fixing this error: `Unhandled Runtime Error: ReferenceError: startOf

--- src/components/vacations/annual-vacation-quadrant.tsx
+++ src/components/vacations/annual-vacation-quadrant.tsx
@@ -4,7 +4,7 @@
 import { useMemo, useState } from 'react';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { useDataProvider } from '@/hooks/use-data-provider';
-import { addWeeks, endOfWeek, format, getISOWeek, getYear, startOfYear, eachDayOfInterval, parseISO, startOfWeek, isBefore, isAfter, getISODay, endOfDay, isSameDay, isWithinInterval } from 'date-fns';
+import { addWeeks, endOfWeek, format, getISOWeek, getYear, startOfYear, eachDayOfInterval, parseISO, startOfWeek, isBefore, isAfter, getISODay, endOfDay, isSameDay, isWithinInterval, startOfDay } from 'date-fns';
 import { cn } from '@/lib/utils';
 import { Skeleton } from '../ui/skeleton';
 import { Users, Clock, PlusCircle, FileDown, Loader2 } from 'lucide-react';
@@ -472,3 +472,5 @@ export function AnnualVacationQuadrant() {
         </Card>
     );
 }
+
+    
