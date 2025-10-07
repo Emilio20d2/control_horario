83ef521c8f9b6f09e3a30afab1152938feef3be0
Emilio Gómez Pardo <emilio20d2@gmail.com>
2025-10-07 12:35:49 +0000

    cambia el ordende las tarjetas de la pagina vacaciones. la de programar

--- src/app/vacations/page.tsx
+++ src/app/vacations/page.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import { AnnualVacationQuadrant } from '@/components/vacations/annual-vacation-quadrant';
@@ -9,8 +8,8 @@ export default function VacationsPage() {
     return (
         <div className="flex flex-col gap-6">
             <div className="px-4 md:px-6 pt-4 space-y-6">
-                <AnnualVacationQuadrant />
                 <VacationPlanner />
+                <AnnualVacationQuadrant />
             </div>
         </div>
     )
