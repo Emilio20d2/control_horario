51ae1af084dfbea4e1527b2edbb6486d64d0f340
Emilio Gómez Pardo <emilio20d2@gmail.com>
2025-10-23 22:44:52 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,9 +1,11 @@
-
-
-import { initializeApp, getApps, getApp } from 'firebase/app';
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBoZTFnt7kh_moxTzuS5zR1LNe86nvuOs4",
   authDomain: "shiftmaster-9iefy.firebaseapp.com",
@@ -14,30 +16,10 @@ const firebaseConfig = {
   appId: "1:904324719398:web:44d3e5892231f82378d7e7"
 };
 
-
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
-
-// Connect to the default database
 const db = getFirestore(app);
 const auth = getAuth(app);
 
 
 export { app, db, auth, firebaseConfig };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
