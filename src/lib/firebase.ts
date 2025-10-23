9c710c8a1de34292ebb6a7192b8199d08a0771b3
Emilio Gómez Pardo <emilio20d2@gmail.com>
2025-10-23 22:31:11 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  "projectId": "shiftmaster-9iefy",
-  "appId": "1:904324719398:web:448d7c2c842fd59778d7e7",
-  "storageBucket": "shiftmaster-9iefy.appspot.com",
-  "apiKey": "AIzaSyBoZTFnt7kh_moxTzuS5zR1LNe86nvuOs4",
-  "authDomain": "shiftmaster-9iefy.firebaseapp.com",
-  "messagingSenderId": "904324719398"
+  apiKey: "AIzaSyBoZTFnt7kh_moxTzuS5zR1LNe86nvuOs4",
+  authDomain: "shiftmaster-9iefy.firebaseapp.com",
+  databaseURL: "https://shiftmaster-9iefy-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "shiftmaster-9iefy",
+  storageBucket: "shiftmaster-9iefy.appspot.com",
+  messagingSenderId: "904324719398",
+  appId: "1:904324719398:web:44d3e5892231f82378d7e7"
 };
 
 
@@ -23,6 +24,7 @@ const auth = getAuth(app);
 
 
 export { app, db, auth, firebaseConfig };
+
 
 
 
