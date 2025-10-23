64afd2247c11abddc553d38904891f9cfae2a4da
Emilio Gómez Pardo <emilio20d2@gmail.com>
2025-10-23 16:14:17 +0000

    cuando resaltes los botones invierte los colores

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-white font-semibold border border-primary hover:bg-primary/90",
+        default: "bg-primary text-white font-semibold border border-primary hover:bg-background hover:text-primary",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
