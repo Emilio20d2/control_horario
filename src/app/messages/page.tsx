37aefccc631948dda41266f7bceb3a003ace9f0d
Emilio Gómez Pardo <emilio20d2@gmail.com>
2025-10-25 09:07:16 +0000

    en la pagina mensajes haz la columna del chat mas estrecha

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -196,11 +196,11 @@ export default function MessagesPage() {
     }
     
     return (
-        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 h-full">
-            <div className="col-span-1">
+        <div className="grid grid-cols-1 md:grid-cols-5 gap-4 p-4 h-full">
+            <div className="md:col-span-2">
                  <ConversationList />
             </div>
-            <div className="md:col-span-2 lg:col-span-3">
+            <div className="md:col-span-3">
                 <ChatView />
             </div>
         </div>
